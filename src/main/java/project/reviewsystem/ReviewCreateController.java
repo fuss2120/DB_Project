f47e95a4e29ac10dededc00486d55722d4662e47
Scott Coyne <scoyne@tythonus.mscsnet.mu.edu>
2019-12-02 18:21:34 -0600

    add hidden email field to form

--- src/main/java/project/reviewsystem/ReviewCreateController.java
+++ src/main/java/project/reviewsystem/ReviewCreateController.java
@@ -3,6 +3,8 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import project.reviewsystem.domain.Paper;
 import project.reviewsystem.domain.Rating;
+import project.reviewsystem.domain.Participator;
 import project.reviewsystem.service.PaperService;
 import project.reviewsystem.service.RatingService;
 
@@ -28,7 +31,9 @@ public class ReviewCreateController {
 	private static final Logger logger = LoggerFactory.getLogger(ReviewCreateController.class);
     
     @GetMapping("/reviewpage")
-    public String reviewpage(Model model) {
+    public String reviewpage(Model model, HttpSession session) {
+        Participator user = (Participator)session.getAttribute("user");
+        model.addAttribute("user", user);
         return "reviewpage";
     }
 	
